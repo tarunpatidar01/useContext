a96605043e2a144729e1b040e8321e7f58c0218a
tarunpatidar01 <tarunpatidar09427@gmail.com>
2024-10-18 15:28:51 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
+import './app.css'
+import {  useState } from "react"
+import ChildA from "./components/ChildA";
+import { createContext } from "react";
 
-import { createContext, useState } from 'react'
-import ChildA from './components/ChildA';
+const ThemeContext = createContext();
+
+
 
 // Step 1: Create Context
 const UserContext =  createContext();
-
 //Step 2: wrap all the child inside a provider
-
 //Step 3: pass value
-
 //step 4 : consumer k andar jakar consume karlo
 
 function App() {
   const [user, setUser] = useState({name:"Tarun"})
+
+  const [theme, setTheme] = useState('light')
   return (
-    <>
     <UserContext.Provider value={user}>
-      <ChildA/>
-    </UserContext.Provider>
-    </>
+
+    
+ <ThemeContext.Provider value={{theme, setTheme}}>
+  <div id="conatiner" style={{backgroundColor:theme==='light'?"beige":"black"}}>
+  <ChildA/>
+  
+  </div>
+  
+ </ThemeContext.Provider>
+ </UserContext.Provider>
+
+
+
+
+
+    // <>
+    
   )
 }
 
 export default App
 export {UserContext}
+export {ThemeContext}
--- src/components/ChildC.jsx
+++ src/components/ChildC.jsx
@@ -1,11 +1,23 @@
 import React, { useContext } from 'react'
+import { ThemeContext } from '../App'
 import { UserContext } from '../App'
 
 function ChildC() {
     const user = useContext(UserContext);
+    const {theme, setTheme} = useContext(ThemeContext);
+    function handleclick(){
+   if(theme === 'light'){
+    setTheme('dark')
+   }else{
+    setTheme('light')
+   }
+    }
   return (
     <div>
+      
+      {theme}
       <h1>Hello {user.name}</h1>
+      <button onClick={handleclick}>CHnage Theme</button>
     </div>
   )
 }
